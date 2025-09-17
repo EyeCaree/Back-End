55b7b3b937c4c1eb41794bce08026c5632382a55
WilisArum02 <wilissarrum@gmail.com>
2025-09-17 16:21:53 +0700

    tes upload

--- config/cors.php
+++ config/cors.php
@@ -6,12 +6,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' =>
-    [
-    'http://localhost:5173',
-    'http://192.168.1.136:3000',
-    'https://d4a2-36-66-204-109.ngrok-free.app'
-    ],
+    'allowed_origins' => ['*'],
+    // [
+    // 'http://localhost:5173',
+    // 'http://192.168.1.136:3000',
+    // 'http://794a-36-66-204-109.ngrok-free.app'
+    // ],
 
     'allowed_origins_patterns' => [],
 
